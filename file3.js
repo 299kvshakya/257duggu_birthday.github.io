e2b51c0a4c051806fa3eeec4ce3bf83bcbc6415d
vishal_k_shakya <89843988+299kvshakya@users.noreply.github.com>
2021-09-21 01:32:51 +0530

    Add files via upload

---
+++ file3.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Pal Pal Dil Ke Paas",
+      artist: "BEST FRIEND",
+      img: "img1",
+      src: "song1 pal pal dil ke pass"
+    },
+    {
+      name: "Sach Bolu Toh Milkr Tumse",
+      artist: "BEST FRIEND",
+      img: "img2",
+      src: "song2 socho na jara ye"
+    },
+    {
+      name: "Apne Toh Apne Hote Hain",
+      artist: "BEST FRIEND",
+      img: "img3",
+      src: "song 3 apne to apne hote hai"
+    },
+    {
+      name: "Phir Mulakat Hogi Kabhi",
+      artist: "BEST FRIEND",
+      img: "img4",
+      src: "song4 phir mulakat hogi kabhi"
+    },
+    {
+      name: "Tumse Milna",
+      artist: "BEST FRIEND",
+      img: "img5",
+      src: "song 5 tumse milna"
+    },
+    {
+      name: "Chaand Sitare",
+      artist: "BEST FRIEND",
+      img: "img8",
+      src: "song 6 chaand sitare"
+    },
+    {
+      name: "Aur Iss Dil Me ",
+      artist: "BEST FRIEND",
+      img: "img6",
+      src: "song 7 aur iss dil me"
+    },
+    {
+      name: "Na Gal Meri Bas Di Rahi",
+      artist: "BEST FRIEND",
+      img: "img7",
+      src: "song 8 na gal meri bas di rahi"
+    },
+    {
+      name: "Pal bhar",
+      artist: "BEST FRIEND",
+      img: "img9",
+      src: "song 9 Pal Bhar"
+    },
+    {
+      name: "Muskurane Ki Wajah",
+      artist: "BEST FRIEND",
+      img: "img10",
+      src: "song 10 Muskurane Ki Wajah"
+    },
+    {
+      name: "Shayad Fir Se",
+      artist: "BEST FRIEND",
+      img: "img11",
+      src: "song 11 Shayad Fir Se"
+    },
+    {
+      name: "O Sajnaa",
+      artist: "BEST FRIEND",
+      img: "img12",
+      src: "song 12 O Sajnaa"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
